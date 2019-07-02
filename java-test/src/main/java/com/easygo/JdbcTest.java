c29d7a20c37609d670f4e7b1cf3bdc5ccfe33206
supnge <supeng87@126.com>
2019-07-03 00:55:31 +0800

    初始提交

---
+++ java-test/src/main/java/com/easygo/JdbcTest.java
@@ -0,0 +1,55 @@
+package com.easygo;
+
+import java.sql.*;
+
+public class JdbcTest {
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test1?useUnicode=true&characterEncoding=utf8", "root", "Sp_123456");
+
+            statement = connection.createStatement();
+            String sql = "select * from user;";
+
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getObject(1));
+                System.out.println(resultSet.getObject(2));
+                System.out.println(resultSet.getObject(3));
+                System.out.println(resultSet.getObject(4));
+                System.out.println(resultSet.getObject(5));
+                System.out.println("===============================");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
